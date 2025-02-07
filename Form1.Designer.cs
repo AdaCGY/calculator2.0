c922c58069e7170652786c2ee59aa2eeec648026
Ada Chen <ch46234881@gmail.com>
2025-02-07 14:21:25 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace 計算機renew
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttondelete = new System.Windows.Forms.Button();
+            this.richTextBox紀錄 = new System.Windows.Forms.RichTextBox();
+            this.button等於 = new System.Windows.Forms.Button();
+            this.labelresult = new System.Windows.Forms.Label();
+            this.buttonN3 = new System.Windows.Forms.Button();
+            this.buttonN6 = new System.Windows.Forms.Button();
+            this.button除 = new System.Windows.Forms.Button();
+            this.button乘 = new System.Windows.Forms.Button();
+            this.button減 = new System.Windows.Forms.Button();
+            this.button加 = new System.Windows.Forms.Button();
+            this.buttonN9 = new System.Windows.Forms.Button();
+            this.buttonN0 = new System.Windows.Forms.Button();
+            this.buttonN2 = new System.Windows.Forms.Button();
+            this.buttonN5 = new System.Windows.Forms.Button();
+            this.buttonN8 = new System.Windows.Forms.Button();
+            this.button點 = new System.Windows.Forms.Button();
+            this.buttonAC = new System.Windows.Forms.Button();
+            this.buttonN1 = new System.Windows.Forms.Button();
+            this.buttonN4 = new System.Windows.Forms.Button();
+            this.buttonN7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttondelete
+            // 
+            this.buttondelete.Location = new System.Drawing.Point(16, 367);
+            this.buttondelete.Name = "buttondelete";
+            this.buttondelete.Size = new System.Drawing.Size(67, 29);
+            this.buttondelete.TabIndex = 44;
+            this.buttondelete.Text = "delete";
+            this.buttondelete.UseVisualStyleBackColor = true;
+            this.buttondelete.Click += new System.EventHandler(this.buttondelete_Click);
+            // 
+            // richTextBox紀錄
+            // 
+            this.richTextBox紀錄.Location = new System.Drawing.Point(377, 119);
+            this.richTextBox紀錄.Name = "richTextBox紀錄";
+            this.richTextBox紀錄.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.richTextBox紀錄.Size = new System.Drawing.Size(613, 231);
+            this.richTextBox紀錄.TabIndex = 43;
+            this.richTextBox紀錄.Text = "";
+            // 
+            // button等於
+            // 
+            this.button等於.Location = new System.Drawing.Point(377, 367);
+            this.button等於.Name = "button等於";
+            this.button等於.Size = new System.Drawing.Size(119, 66);
+            this.button等於.TabIndex = 42;
+            this.button等於.Text = "等於";
+            this.button等於.UseVisualStyleBackColor = true;
+            this.button等於.Click += new System.EventHandler(this.button等於_Click);
+            // 
+            // labelresult
+            // 
+            this.labelresult.AutoSize = true;
+            this.labelresult.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelresult.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelresult.Location = new System.Drawing.Point(22, 48);
+            this.labelresult.Name = "labelresult";
+            this.labelresult.Size = new System.Drawing.Size(89, 30);
+            this.labelresult.TabIndex = 41;
+            this.labelresult.Text = "label1";
+            // 
+            // buttonN3
+            // 
+            this.buttonN3.Location = new System.Drawing.Point(189, 284);
+            this.buttonN3.Name = "buttonN3";
+            this.buttonN3.Size = new System.Drawing.Size(67, 66);
+            this.buttonN3.TabIndex = 39;
+            this.buttonN3.Text = "3";
+            this.buttonN3.UseVisualStyleBackColor = true;
+            this.buttonN3.Click += new System.EventHandler(this.buttonN3_Click);
+            // 
+            // buttonN6
+            // 
+            this.buttonN6.Location = new System.Drawing.Point(189, 202);
+            this.buttonN6.Name = "buttonN6";
+            this.buttonN6.Size = new System.Drawing.Size(67, 66);
+            this.buttonN6.TabIndex = 38;
+            this.buttonN6.Text = "6";
+            this.buttonN6.UseVisualStyleBackColor = true;
+            this.buttonN6.Click += new System.EventHandler(this.buttonN6_Click);
+            // 
+            // button除
+            // 
+            this.button除.Location = new System.Drawing.Point(279, 367);
+            this.button除.Name = "button除";
+            this.button除.Size = new System.Drawing.Size(67, 66);
+            this.button除.TabIndex = 37;
+            this.button除.Text = "除";
+            this.button除.UseVisualStyleBackColor = true;
+            this.button除.Click += new System.EventHandler(this.button除_Click);
+            // 
+            // button乘
+            // 
+            this.button乘.Location = new System.Drawing.Point(279, 284);
+            this.button乘.Name = "button乘";
+            this.button乘.Size = new System.Drawing.Size(67, 66);
+            this.button乘.TabIndex = 36;
+            this.button乘.Text = "乘";
+            this.button乘.UseVisualStyleBackColor = true;
+            this.button乘.Click += new System.EventHandler(this.button乘_Click);
+            // 
+            // button減
+            // 
+            this.button減.Location = new System.Drawing.Point(279, 202);
+            this.button減.Name = "button減";
+            this.button減.Size = new System.Drawing.Size(67, 66);
+            this.button減.TabIndex = 35;
+            this.button減.Text = "減";
+            this.button減.UseVisualStyleBackColor = true;
+            this.button減.Click += new System.EventHandler(this.button減_Click);
+            // 
+            // button加
+            // 
+            this.button加.Location = new System.Drawing.Point(279, 119);
+            this.button加.Name = "button加";
+            this.button加.Size = new System.Drawing.Size(67, 66);
+            this.button加.TabIndex = 25;
+            this.button加.Text = "加";
+            this.button加.UseVisualStyleBackColor = true;
+            this.button加.Click += new System.EventHandler(this.button加_Click);
+            // 
+            // buttonN9
+            // 
+            this.buttonN9.Location = new System.Drawing.Point(189, 119);
+            this.buttonN9.Name = "buttonN9";
+            this.buttonN9.Size = new System.Drawing.Size(67, 66);
+            this.buttonN9.TabIndex = 33;
+            this.buttonN9.Text = "9";
+            this.buttonN9.UseVisualStyleBackColor = true;
+            this.buttonN9.Click += new System.EventHandler(this.buttonN9_Click);
+            // 
+            // buttonN0
+            // 
+            this.buttonN0.Location = new System.Drawing.Point(102, 367);
+            this.buttonN0.Name = "buttonN0";
+            this.buttonN0.Size = new System.Drawing.Size(67, 66);
+            this.buttonN0.TabIndex = 32;
+            this.buttonN0.Text = "0";
+            this.buttonN0.UseVisualStyleBackColor = true;
+            this.buttonN0.Click += new System.EventHandler(this.buttonN0_Click);
+            // 
+            // buttonN2
+            // 
+            this.buttonN2.Location = new System.Drawing.Point(102, 284);
+            this.buttonN2.Name = "buttonN2";
+            this.buttonN2.Size = new System.Drawing.Size(67, 66);
+            this.buttonN2.TabIndex = 31;
+            this.buttonN2.Text = "2";
+            this.buttonN2.UseVisualStyleBackColor = true;
+            this.buttonN2.Click += new System.EventHandler(this.buttonN2_Click);
+            // 
+            // buttonN5
+            // 
+            this.buttonN5.Location = new System.Drawing.Point(102, 202);
+            this.buttonN5.Name = "buttonN5";
+            this.buttonN5.Size = new System.Drawing.Size(67, 66);
+            this.buttonN5.TabIndex = 30;
+            this.buttonN5.Text = "5";
+            this.buttonN5.UseVisualStyleBackColor = true;
+            this.buttonN5.Click += new System.EventHandler(this.buttonN5_Click);
+            // 
+            // buttonN8
+            // 
+            this.buttonN8.Location = new System.Drawing.Point(102, 119);
+            this.buttonN8.Name = "buttonN8";
+            this.buttonN8.Size = new System.Drawing.Size(67, 66);
+            this.buttonN8.TabIndex = 29;
+            this.buttonN8.Text = "8";
+            this.buttonN8.UseVisualStyleBackColor = true;
+            this.buttonN8.Click += new System.EventHandler(this.buttonN8_Click);
+            // 
+            // button點
+            // 
+            this.button點.Location = new System.Drawing.Point(189, 382);
+            this.button點.Name = "button點";
+            this.button點.Size = new System.Drawing.Size(67, 51);
+            this.button點.TabIndex = 28;
+            this.button點.Text = "小數點";
+            this.button點.UseVisualStyleBackColor = true;
+            this.button點.Click += new System.EventHandler(this.button點_Click);
+            // 
+            // buttonAC
+            // 
+            this.buttonAC.Location = new System.Drawing.Point(16, 402);
+            this.buttonAC.Name = "buttonAC";
+            this.buttonAC.Size = new System.Drawing.Size(67, 31);
+            this.buttonAC.TabIndex = 27;
+            this.buttonAC.Text = "AC";
+            this.buttonAC.UseVisualStyleBackColor = true;
+            this.buttonAC.Click += new System.EventHandler(this.buttonAC_Click);
+            // 
+            // buttonN1
+            // 
+            this.buttonN1.Location = new System.Drawing.Point(16, 284);
+            this.buttonN1.Name = "buttonN1";
+            this.buttonN1.Size = new System.Drawing.Size(67, 66);
+            this.buttonN1.TabIndex = 26;
+            this.buttonN1.Text = "1";
+            this.buttonN1.UseVisualStyleBackColor = true;
+            this.buttonN1.Click += new System.EventHandler(this.buttonN1_Click);
+            // 
+            // buttonN4
+            // 
+            this.buttonN4.Location = new System.Drawing.Point(16, 202);
+            this.buttonN4.Name = "buttonN4";
+            this.buttonN4.Size = new System.Drawing.Size(67, 66);
+            this.buttonN4.TabIndex = 40;
+            this.buttonN4.Text = "4";
+            this.buttonN4.UseVisualStyleBackColor = true;
+            this.buttonN4.Click += new System.EventHandler(this.buttonN4_Click);
+            // 
+            // buttonN7
+            // 
+            this.buttonN7.Location = new System.Drawing.Point(16, 119);
+            this.buttonN7.Name = "buttonN7";
+            this.buttonN7.Size = new System.Drawing.Size(67, 66);
+            this.buttonN7.TabIndex = 34;
+            this.buttonN7.Text = "7";
+            this.buttonN7.UseVisualStyleBackColor = true;
+            this.buttonN7.Click += new System.EventHandler(this.buttonN7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 451);
+            this.Controls.Add(this.buttondelete);
+            this.Controls.Add(this.richTextBox紀錄);
+            this.Controls.Add(this.button等於);
+            this.Controls.Add(this.labelresult);
+            this.Controls.Add(this.buttonN3);
+            this.Controls.Add(this.buttonN6);
+            this.Controls.Add(this.button除);
+            this.Controls.Add(this.button乘);
+            this.Controls.Add(this.button減);
+            this.Controls.Add(this.button加);
+            this.Controls.Add(this.buttonN9);
+            this.Controls.Add(this.buttonN0);
+            this.Controls.Add(this.buttonN2);
+            this.Controls.Add(this.buttonN5);
+            this.Controls.Add(this.buttonN8);
+            this.Controls.Add(this.button點);
+            this.Controls.Add(this.buttonAC);
+            this.Controls.Add(this.buttonN1);
+            this.Controls.Add(this.buttonN4);
+            this.Controls.Add(this.buttonN7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttondelete;
+        private System.Windows.Forms.RichTextBox richTextBox紀錄;
+        private System.Windows.Forms.Button button等於;
+        private System.Windows.Forms.Label labelresult;
+        private System.Windows.Forms.Button buttonN3;
+        private System.Windows.Forms.Button buttonN6;
+        private System.Windows.Forms.Button button除;
+        private System.Windows.Forms.Button button乘;
+        private System.Windows.Forms.Button button減;
+        private System.Windows.Forms.Button button加;
+        private System.Windows.Forms.Button buttonN9;
+        private System.Windows.Forms.Button buttonN0;
+        private System.Windows.Forms.Button buttonN2;
+        private System.Windows.Forms.Button buttonN5;
+        private System.Windows.Forms.Button buttonN8;
+        private System.Windows.Forms.Button button點;
+        private System.Windows.Forms.Button buttonAC;
+        private System.Windows.Forms.Button buttonN1;
+        private System.Windows.Forms.Button buttonN4;
+        private System.Windows.Forms.Button buttonN7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 計算機renew
+{
+    public partial class Form1 : Form
+    {
+        List<string> NumberList_string = new List<string>(); //存取被點擊的數字字串
+        List<decimal> NumberList_decimal = new List<decimal>(); //存取數字字串轉型
+        List<string> OperatorList = new List<string>(); //存取被點擊的運算符號(加減乘除)
+        List<string>HistoryList = new List<string>(); //存取所有被點擊的按鈕  
+        string add = "+";
+        string subtract = "-";
+        string multiply = "x";
+        string divide = "÷";
+        string decimalpoint = ".";
+        string Number; //用以數字處理
+        decimal Result;//儲存結果
+        public Form1()
+        {
+            InitializeComponent();
+            labelresult.Text = string.Empty;
+        }
+
+        private void buttonN0_Click(object sender, EventArgs e)
+        {
+            Number_Input("0");
+        }
+
+        private void buttonN1_Click(object sender, EventArgs e)
+        {
+            Number_Input("1");
+        }
+
+        private void buttonN2_Click(object sender, EventArgs e)
+        {
+            Number_Input("2");
+        }
+
+        private void buttonN3_Click(object sender, EventArgs e)
+        {
+            Number_Input("3");
+        }
+
+        private void buttonN4_Click(object sender, EventArgs e)
+        {
+            Number_Input("4");
+        }
+
+        private void buttonN5_Click(object sender, EventArgs e)
+        {
+            Number_Input("5");
+        }
+
+        private void buttonN6_Click(object sender, EventArgs e)
+        {
+            Number_Input("6");
+        }
+
+        private void buttonN7_Click(object sender, EventArgs e)
+        {
+            Number_Input("7");
+        }
+
+        private void buttonN8_Click(object sender, EventArgs e)
+        {
+            Number_Input("8");
+        }
+
+        private void buttonN9_Click(object sender, EventArgs e)
+        {
+            Number_Input("9");
+        }
+
+        private void button加_Click(object sender, EventArgs e)
+        {
+            Operator_Input(add);
+        }
+
+        private void button減_Click(object sender, EventArgs e)
+        {
+            Operator_Input(subtract);
+        }
+
+        private void button乘_Click(object sender, EventArgs e)
+        {
+            Operator_Input(multiply);
+        }
+
+        private void button除_Click(object sender, EventArgs e)
+        {
+            Operator_Input(divide);
+        }
+
+        private void button點_Click(object sender, EventArgs e)
+        {
+            Decimal_Point_Input(decimalpoint);
+        }
+
+        private void buttondelete_Click(object sender, EventArgs e)
+        {
+            DeleteBottonClick();
+        }
+
+        private void buttonAC_Click(object sender, EventArgs e)
+        {
+            ResetBottonClick();
+        }
+
+        private void button等於_Click(object sender, EventArgs e)
+        {
+            EqualBottonClick();
+        }
+        public void Number_Input(string input)
+        {
+            HistoryList.Add(input);
+            labelresult.Text += input;
+        }
+        public void Decimal_Point_Input(string input)
+        {
+            if (labelresult.Text == string.Empty)
+            {
+                throw new Exception("請先輸入數字再點擊小數點");
+            }
+            else
+            {
+                if (labelresult.Text.Last() == '.')
+                {
+                    return;
+                }
+                HistoryList.Add(input);
+                labelresult.Text += input;
+            }
+        }
+        public void Operator_Input(string input)
+        {
+            if (labelresult.Text == string.Empty)
+            {
+                throw new Exception("請先輸入數字再點擊運算符號");
+            }
+            else
+            {
+                if (labelresult.Text.Last() == '+' || labelresult.Text.Last() == '-' || labelresult.Text.Last() == 'x' || labelresult.Text.Last() == '÷' || labelresult.Text.Last() == '.')
+                {
+                    return;
+                }
+                HistoryList.Add(input);
+                labelresult.Text += input;
+            }
+        }
+        public void EqualBottonClick()
+        {
+            if (labelresult.Text.Contains("="))
+            {
+                return;
+            }
+            foreach (var item in HistoryList) //混合數字與運算符號 >>符號夾雜在數字之間，數字列表總數永遠大於運算符號列表總數一個
+            {
+
+                if (item != add && item != subtract && item != multiply && item != divide)
+                {
+                    Number += item;
+                }
+                if (item == add || item == subtract || item == multiply || item == divide)
+                {
+                    OperatorList.Add(item);
+                    NumberList_string.Add(Number);
+                    Number = string.Empty;
+                }
+            }
+            if (Number != string.Empty) //收錄最後一個符號後面的數字
+            {
+                NumberList_string.Add(Number);
+                Number = string.Empty;
+            }
+            foreach (var item in NumberList_string) //把數字轉型
+            {
+                var b = decimal.TryParse(item, out decimal number);
+                if (b == true)
+                {
+                    NumberList_decimal.Add(number);
+                }
+            }
+            if (NumberList_decimal.Count >= 2)
+            {
+                Result = NumberList_decimal[0];
+                for (int i = 0; i < OperatorList.Count; i++) //數字總有12個，運算符號就會只有11個
+                {
+                    if (OperatorList[i] == add)
+                    {
+                        Result += NumberList_decimal[i + 1];
+                    }
+                    else if (OperatorList[i] == subtract)
+                    {
+                        Result -= NumberList_decimal[i + 1];
+                    }
+                    else if (OperatorList[i] == multiply)
+                    {
+                        Result *= NumberList_decimal[i + 1];
+                    }
+                    else if (OperatorList[i] == divide)
+                    {
+                        if (NumberList_decimal[i + 1] != 0)
+                        {
+                            Result /= NumberList_decimal[i + 1];
+                        }
+                        else
+                        {
+                            throw new Exception("除數不可為0");
+                        }
+                    }
+                }
+                labelresult.Text += "=" + Result;
+                richTextBox紀錄.Text += labelresult.Text + "\n";
+            }
+            else
+            {
+                throw new Exception("計算數值未達兩項，無法進行計算");
+            }
+        }
+        public void DeleteBottonClick()
+        {
+            if (labelresult.Text.Length > 0 && HistoryList.Count>0)
+            {
+                HistoryList.RemoveAt(HistoryList.Count-1);
+                var length = labelresult.Text.Length;
+                labelresult.Text= labelresult.Text.Remove(length-1,1);
+            }
+        }
+        public void ResetBottonClick()
+        {
+            NumberList_string.Clear();
+            NumberList_decimal.Clear();
+            OperatorList.Clear();
+            HistoryList.Clear();
+            labelresult.Text= string.Empty;
+        }
+    }
+}
